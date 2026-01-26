015501a5a59735d267122ca870aa39434c613ede
woo-hyun <kwh77974481@gmail.com>
2026-01-26 18:54:55 +0900

    Fix language-toggle test selector to match successful pattern
    - Change from #mantine-r0-target to [aria-label='Display Language']
    - Use successful pattern from language-toggle-test.spec.ts
    - Add more failover selectors for better reliability

--- tests/logged-out/language-toggle.spec.ts
+++ tests/logged-out/language-toggle.spec.ts
@@ -24,19 +24,32 @@ test(title, details, async ({ page }) => {
   await page.goto('https://dev-dashboard.immerse.online/login');
   // Clicking on the language selector to open the language options.
   await page
-    .find('#mantine-r0-target', {
+    .find("[aria-label='Display Language']", {
       failover: [
-        "[aria-label='Display Language']",
         "(.//div[normalize-space(.)='ENGLISH'])[1]",
+        'div.css-18wbxrz',
+        'div.css-1a47ai3 > div:nth-of-type(2)',
+        'div.c-jhyvPY > div:nth-of-type(1) > div:nth-of-type(2)',
+        'div.c-gqwkJN > div > div:nth-of-type(1) > div:nth-of-type(2)',
+        'div.c-ejwOqd > div > div > div:nth-of-type(1) > div:nth-of-type(2)',
+        'html > body > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(2)',
+        'body > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(2)',
+        ".//div[normalize-space(.)='ENGLISH']",
       ],
     })
     .click();
   // Selecting Japanese from the language options to change the display language.
   await page
-    .find('#mantine-r0-dropdown > div > button:nth-of-type(2)', {
+    .find(".//button[normalize-space(.)='日本語']", {
       failover: [
-        ".//button[normalize-space(.)='日本語']",
         'div.css-1a47ai3 > div:nth-of-type(3) > div > button:nth-of-type(2)',
+        'div.c-jhyvPY > div:nth-of-type(1) > div:nth-of-type(3) > div > button:nth-of-type(2)',
+        'div.c-gqwkJN > div > div:nth-of-type(1) > div:nth-of-type(3) > div > button:nth-of-type(2)',
+        'div.c-ejwOqd > div > div > div:nth-of-type(1) > div:nth-of-type(3) > div > button:nth-of-type(2)',
+        'body > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(3) > div > button:nth-of-type(2)',
+        "[role='menuitem']",
+        "[data-menu-item='true']",
+        'button.mantine-Menu-item',
       ],
     })
     .click();
